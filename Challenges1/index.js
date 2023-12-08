ad5275fb5e3765c7932451ae7f76e1dc1810afb6
Mudpadans <philmodel.101@gmail.com>
2023-12-07 21:14:04 -0500

    init

---
+++ Challenges1/index.js
@@ -0,0 +1,25 @@
+//A greeting function
+
+// function greet(name){
+//   return `Hello ${name}, how are you doing today?`
+// }
+
+// console.log(greet("Kristina"))
+
+//a population function that measure growth or decline in a town
+
+// function nbYear(p0, percent, aug, p) {
+//     let years = 0;
+
+//     while (p0 < p) {
+//       p0 += p0 * (percent / 100) + aug;
+//       years++;
+//     }
+
+//     return years;
+// }
+
+// console.log(nbYear(1500000, 0.25, 1000, 2000000))
+
+//A vowel count function
+
